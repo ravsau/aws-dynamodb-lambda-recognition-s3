867163dda7efd8a8ac49b7c4d9666748f1a27cc2
saurav <saurav@Robs-MacBook-Pro.local>
2018-05-04 19:06:54 -0400

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aws-dynamodb-lambda-recognition-s3
-code for a simple  lambda function which stores the labels for in image in DynamoDB table 
+
+Code for a simple  lambda function which stores the labels for in image in DynamoDB table.
+
+## AWS services used
+1) AWS Lambda
+2) Amazon S3: You have to change the bucket name in the function
+3) DynamoDB
+4) Amazon Rekognition: This is where the magic happens. Rekognition is an Image recognition service which provides a simple API that let's us find labels(car, person), detect faces, detect celebrities etc. We use this service to find out the items/labels in an image.
+
+
+
+ 
---
+++ lambda_function.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+
+import boto3
+from decimal import Decimal
+import json
+import urllib
+
+print('Loading function')
+
+rekognition = boto3.client('rekognition')
+
+dynamo= boto3.resource('dynamodb')
+
+
+
+def detect_labels(bucket, key):
+    response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+
+    
+    return response
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    '''Demonstrates S3 trigger that uses
+    Rekognition APIs to detect faces, labels and index faces in S3 Object.
+    '''
+    #print("Received event: " + json.dumps(event, indent=2))
+
+   
+    
+    print (event)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+    try:
+        
+
+        # Calls rekognition DetectLabels API to detect labels in S3 object
+        response = detect_labels(bucket, key)
+        
+        tosend=""
+        toDynamo=[]
+        
+        for Label in response["Labels"]:
+           # print (Label["Name"] + Label["Confidence"])
+            
+            print ('{0} - {1}%'.format(Label["Name"], Label["Confidence"]))
+            tosend+= '{0} - {1}% '.format(Label["Name"], Label["Confidence"])
+            
+            Name= Label["Name"]
+            Confidence = Label["Confidence"]
+            if Confidence>70:
+                toDynamo.append ( Name)
+            
+            
+    
+     
+        
+        table=dynamo.Table('Dynamo-Lambda')
+        sendDynamo= table.put_item(
+    TableName='Dynamo-Lambda',
+    Item= {"key" : key , "Labels": toDynamo }) 
+    
+        return "hello"
+    except Exception as e:
+       
+        raise e
+
